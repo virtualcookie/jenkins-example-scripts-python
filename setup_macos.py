ccc39e32ad6c2465b868ee72ba25c70e3543b8a5
Himanshu Singh <51034346+virtualcookie@users.noreply.github.com>
2023-09-08 12:08:15 -0700

    Create setup_macos.py

---
+++ setup_macos.py
@@ -0,0 +1,7 @@
+# Run the macos setup script (mock)
+
+print("Running setup_macos.py")
+# output a string to a file
+with open("setup_macos.txt", "w") as f:
+    f.write("Running setup_macos.py\n")
+    f.close()
