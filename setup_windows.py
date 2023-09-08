4ba0e9feefba9f1c49da5c17e9f9824dab8f1a96
Himanshu Singh <51034346+virtualcookie@users.noreply.github.com>
2023-09-08 12:09:11 -0700

    Create setup_windows.py

---
+++ setup_windows.py
@@ -0,0 +1,7 @@
+# Run the windows setup script (mock)
+
+print("Running setup_windows.py")
+# output a string to a file
+with open("setup_macos.txt", "w") as f:
+    f.write("Running setup_windows.py\n")
+    f.close()
